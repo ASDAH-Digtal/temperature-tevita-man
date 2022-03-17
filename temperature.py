75304d4f022107a854c86f295a103b48d517ce7d
tevita-man <tevita_man@student.asdah.school.nz>
2022-03-17 14:50:54 +1300

    Added coment, did 2 of the tasks

--- temperature.py
+++ temperature.py
@@ -1,11 +1,11 @@
-# Student Name:
-# Date: 
+# Student Name: Tevita Manu
+# Date: 17/03/2022
 
 # 1. Write a print statement that says "Welcome to the Temperature Conversion program"
-print("")
+print("Welcome to the Temperature Conversion program")
 
 # 2. Ask the user if they want to convert from Farenheit to Celsius or vice versa (provide 2 options)
-choice = input("")
+choice = input("Do you want to convert from Farenheit to Celsius, or Celsius to Farenheit ")
 
 # 3. Ask the user for a number to convert
 temp = int(input(""))
